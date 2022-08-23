f7512d2c56eed6cc7413d94e1ef8a9b015742e63
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-08-22 22:35:34 -0400

    Add files via upload

---
+++ Creating cluster chart  with ggplot.R
@@ -0,0 +1,25 @@
+#Creating cluster chart  with ggplot
+### make sure to install all the packages below. 
+pacman::p_load(datasets, pacman, psych, rio, tidyverse)
+
+## Using the mtcars dataset
+?mtcars
+view(mtcars)
+glimpse(mtcars)
+
+attach(mtcars)
+
+data <- mtcars %>% rownames_to_column() %>% as_tibble() %>% 
+  select(car = rowname, mpg:hp, wt, qsec, gear, carb) %>% 
+  mutate_at(vars(-car), scale) %>%
+  print()
+
+### Analyzing data
+### calculating the cluster 
+hie_cluster <- data %>% dist %>% hclust()
+
+### plotting the dendrogram
+hie_cluster%>% plot(labels = data$car, cex= 0.8, hang = -1)
+
+### drawing boxes around cluster
+hie_cluster %>% rect.hclust(k = 5, border = 2.6)
